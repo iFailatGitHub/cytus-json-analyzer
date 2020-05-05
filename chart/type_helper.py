cf1d1e22ad12739a122d410584a23088be594f12
iFailatGithub <ifailatgeemail@gmail.com>
2020-05-05 14:21:06 +0800

    added assertion error messages

--- chart/type_helper.py
+++ chart/type_helper.py
@@ -5,37 +5,39 @@ T = TypeVar("T")
 EnumT = TypeVar("EnumT", bound=Enum)
 
 def from_int(x: Any) -> int:
-    assert isinstance(x, int) and not isinstance(x, bool)
+    assert isinstance(x, int) and not isinstance(x, bool), \
+        f"{x} is not an integer."
     return x
 
 
 def to_enum(c: Type[EnumT], x: Any) -> EnumT:
-    assert isinstance(x, c)
+    assert isinstance(x, c), f"{x} is not an instance of {c}."
     return x.value
 
 
 def from_list(f: Callable[[Any], T], x: Any) -> List[T]:
-    assert isinstance(x, list)
+    assert isinstance(x, list), f"{x} is not a list of {f}."
     return [f(y) for y in x]
 
 
 def to_class(c: Type[T], x: Any) -> dict:
-    assert isinstance(x, c)
+    assert isinstance(x, c), f"{x} is not an instance of {c}"
     return cast(Any, x).to_dict()
 
 
 def from_float(x: Any) -> float:
-    assert isinstance(x, (float, int)) and not isinstance(x, bool)
+    assert isinstance(x, (float, int)) and not isinstance(x, bool), \
+        f"{x} is not a float."
     return float(x)
 
 
 def from_bool(x: Any) -> bool:
-    assert isinstance(x, bool)
+    assert isinstance(x, bool), f"{x} is not a boolean."
     return x
 
 
 def to_float(x: Any) -> float:
-    assert isinstance(x, float)
+    assert isinstance(x, float), f"{x} is not a float."
     return x
 
 def from_str(x: Any) -> str:
@@ -48,8 +50,8 @@ def from_union(fs, x):
             return f(x)
         except:
             pass
-    assert False
+    assert False, f"{x} is not the ff. classes: {fs}"
 
 def from_none(x: Any) -> Any:
-    assert x is None
+    assert x is None, f"{x} is not None."
     return x
